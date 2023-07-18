3a80b16be3cc6e6ec72b43300ea6fdefeb1f3752
dinglide <dinglideday@126.com>
2023-07-18 14:52:51 +0800

    goroutine并发控制

---
+++ src/mutiply/mutiply.go
@@ -0,0 +1,75 @@
+package src
+
+import (
+
+	"fmt"
+	"sync"
+)
+
+func FuncGoroutine() {
+    a := 1
+    // 创建三个无缓冲的 channel，用于协程之间的同步
+    ch1, ch2, ch3, ch4:= make(chan struct{}), make(chan struct{}), make(chan struct{}),make(chan struct{})
+    var wg sync.WaitGroup
+
+    wg.Add(4) // WaitGroup 需要等待三个协程执行结束
+
+    // 启动 goroutine1，使用 ch1 和 ch2 进行同步
+    go goroutine1(&a, ch1, ch2,&wg)
+    // 启动 goroutine2，使用 ch2 和 ch3 进行同步
+    go goroutine2(&a, ch2, ch3,&wg)
+    // 启动 goroutine3，使用 ch3 进行同步
+    go goroutine3(&a, ch3,ch4,&wg)
+	go goroutine4(&a,ch4,&wg)
+
+    ch1 <- struct{}{} // 向 ch1 发送一个空结构体，启动 goroutine1 的执行
+
+    wg.Wait() // 等待所有协程执行结束
+
+    fmt.Println(a)
+}
+
+func goroutine1(i *int, ch1, ch2 chan struct{},wg *sync.WaitGroup) {
+    defer func ()  {
+		close(ch1)
+		wg.Done()	
+	}() // 在 goroutine1 结束时关闭 ch1
+    <-ch1            // 等待 ch1 上接收到数据
+    *i++             // 修改变量 i 的值
+    fmt.Println("goroutine1 ", *i)
+    ch2 <- struct{}{} // 向 ch2 发送一个空结构体，启动 goroutine2 的执行
+}
+
+func goroutine2(i *int, ch2, ch3 chan struct{},wg *sync.WaitGroup) {
+	defer func ()  {
+		close(ch2)
+		wg.Done()	
+	}() // 在 goroutine2 结束时关闭 ch2
+    // defer close(ch2) // 在 goroutine2 结束时关闭 ch2
+    <-ch2            // 等待 ch2 上接收到数据
+    *i *= *i         // 修改变量 i 的值
+    fmt.Println("goroutine2 ", *i)
+    ch3 <- struct{}{} // 向 ch3 发送一个空结构体，启动 goroutine3 的执行
+}
+
+func goroutine3(i *int, ch3,ch4 chan struct{},wg *sync.WaitGroup) {
+	defer func ()  {
+		close(ch3)
+		wg.Done()	
+	}() // 在 goroutine3 结束时关闭 ch3
+    // defer close(ch3) // 在 goroutine3 结束时关闭 ch3
+    <-ch3            // 等待 ch3 上接收到数据
+    *i--             // 修改变量 i 的值
+    fmt.Println("goroutine3 ", *i)
+	ch4<-struct{}{}
+}
+func goroutine4(i *int, ch4 chan struct{},wg *sync.WaitGroup) {
+	defer func ()  {
+		close(ch4)
+		wg.Done()	
+	}() // 在 goroutine3 结束时关闭 ch3
+    // defer close(ch3) // 在 goroutine3 结束时关闭 ch3
+    <-ch4           // 等待 ch3 上接收到数据
+    *i+=10             // 修改变量 i 的值
+    fmt.Println("goroutine4 ", *i)
+}
--- src/practice_test.go
+++ src/practice_test.go
@@ -1,21 +1,27 @@
 package src
 
 import (
-	"fmt"
-	"golangPractice/src/sort"
+	// "fmt"
+	// "golangPractice/src/sort"
+	"golangPractice/src/mutiply"
 	"testing"
 )
 
-func TestQucikSort(t *testing.T) {
-	arr:=[]int{9,0,3,6,-7,12}
-	src.QuickSort(arr,0,len(arr)-1)
-	fmt.Println(arr)
+// func TestQucikSort(t *testing.T) {
+// 	arr:=[]int{9,0,3,6,-7,12}
+// 	src.QuickSort(arr,0,len(arr)-1)
+// 	fmt.Println(arr)
 	
+// }
+
+// func TestMergeSort(t *testing.T) {
+// 	arr:=[]int{9,0,3,6,-7,12}
+// 	res:=src.MergeSort(arr)
+// 	fmt.Println(res)
+	
+// }
+
+func TestFuncGoroutine(t *testing.T) {
+	src.FuncGoroutine()
 }
 
-func TestMergeSort(t *testing.T) {
-	arr:=[]int{9,0,3,6,-7,12}
-	res:=src.MergeSort(arr)
-	fmt.Println(res)
-	
-}
